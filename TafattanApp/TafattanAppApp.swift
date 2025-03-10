63f8df2439ce85fd733d2db9568287251ced274c
asma <Asma.mo@outlook.com>
2025-03-10 12:00:23 +0300

    Initial Commit

---
+++ TafattanApp/TafattanAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TafattanAppApp.swift
+//  TafattanApp
+//
+//  Created by Asma on 10/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct TafattanAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
